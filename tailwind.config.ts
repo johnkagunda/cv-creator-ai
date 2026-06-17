9b10c43f2dd5aad2ee9e6224a5700d7f0692849a
johnkagunda <118008558+johnkagunda@users.noreply.github.com>
2026-06-17 12:32:06 +0300

    Add custom border radius for large size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,7 +63,7 @@ export default {
         },
       },
       borderRadius: {
-        
+         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
